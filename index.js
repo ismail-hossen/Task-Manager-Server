21d6de29dd5f513231bce0d4f0da5c1d8ecc7afb
ismail-hossen <satict2016@gmail.com>
2023-12-22 01:33:19 +0600

    implemented mongodb and create two apis

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000;
+const cors = require("cors");
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(process.env.mongo_url, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const db = await client.db("task-manager");
+    const tasksCollection = await db.collection("tasks");
+
+    app.get("/tasks", async (req, res) => {
+      const param = req.params.email;
+      const result = await tasksCollection.find({ email: param }).toArray();
+      res.send(result);
+    });
+
+    app.post("/create-task", async (req, res) => {
+      const result = await tasksCollection.insertOne(req.body);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
